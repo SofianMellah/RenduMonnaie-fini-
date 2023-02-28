3891f1316ba5d6e5844874e8848cc49d40bbde7f
59013-97-11 <59013-97-11@exchange.ad.afpanet>
2023-02-28 15:02:11 +0100

    et

---
+++ exo4.js
@@ -0,0 +1,29 @@
+let billet10;
+let billet5;
+let piece1;
+function Rendu () {
+    let sommeClient = document.getElementById("SommeClient").value
+    let montantTicket = document.getElementById("Montantticket").value
+    let montantaRendre = sommeClient - montantTicket
+    document.getElementById("Montantarendre").value = montantaRendre
+    billet10 = 0;
+
+    while (montantaRendre >= 10) {
+        billet10++
+        montantaRendre = montantaRendre -10
+    }
+
+    billet5 = 0;
+
+    if (montantaRendre >= 5) {
+        billet5++
+        montantaRendre = montantaRendre -5
+    }
+    
+    piece1 = montantaRendre
+    console.log(billet10,billet5,piece1)
+    document.getElementById("billetsdix").value = billet10
+    document.getElementById("billetscinq").value = billet5
+    document.getElementById("piecesun").value = piece1
+
+}
